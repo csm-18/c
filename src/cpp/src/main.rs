c5919bff484279a651f0ec8c0c6252b58d77d5e2
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-04-05 23:53:29 +0530

    cpp: add version and help commands to cli

--- src/cpp/src/main.rs
+++ src/cpp/src/main.rs
@@ -1,3 +1,17 @@
+
+
+const VERSION:&str = "0.1.0";
 fn main() {
-    println!("Hello from cpp!");
+    //cli args
+    let args: Vec<String>= std::env::args().collect();
+
+    if args.len() == 1 {
+        println!("cpp {VERSION}");
+        println!(" A preprocessor for c language, part of csm-18/c project.");
+        println!("for usage:\n cpp --help");
+    }else if args.len() == 2 && args[1] == "--help" {
+        println!("cpp commands:");
+        println!(" 1.cpp <no args> -> prints cpp version and about message.");
+        println!(" 2.cpp  --help   -> prints this commands list.");
+    }
 }
